1dd71fd51bfbc41ef5a77cd22f4973e3697b62c7
ajithshah <ajithshah37@users.github.com>
2021-02-20 18:28:50 +0530

    --

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname +"/public/index.html");
+    res.sendFile(__dirname +"index.html");
 });
 
-app.listen(3002,function(){
+app.listen(3000,function(){
     console.log("server started on port 3000");
 });
