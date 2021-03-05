a4dd6437c8ef3ef2f24af6b34833a5986e641537
ajithshah <ajithshah37@users.github.com>
2021-03-05 14:39:15 +0530

    --

--- cm_login_reg/usermodel.js
+++ cm_login_reg/usermodel.js
@@ -6,7 +6,7 @@ let userSchema = new Schema({
     userName: { type: String, required: true , trim: true, unique: true },
     emailId: { type: String, match: /\S+@\S+\.\S+/, required: true },
     password: { type :String, required: true },
-    contact: { type: Number, required: true },
+    contact: { type: Number, required: true, },
     location: { type: String, required: true },
     createdOn: { type: Date }
 });
